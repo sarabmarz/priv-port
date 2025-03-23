1c81125388ff8b7e912ea2139984c8e04648c0b2
saara <132011413+sarabmarz@users.noreply.github.com>
2025-03-23 22:59:12 +0000

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -7,27 +7,26 @@ const projectPages = [
   "project5.html"
 ];
 
-// Get the current page filename (without the full path)
+// Get the current page filename
 const currentPage = window.location.pathname.split("/").pop();
 
-// Find the current project's index in the array
+// Find the current project's index
 const currentIndex = projectPages.indexOf(currentPage);
 
 if (currentIndex !== -1) {
-  // Determine Previous and Next Projects
   const prevIndex = currentIndex === 0 ? projectPages.length - 1 : currentIndex - 1;
   const nextIndex = currentIndex === projectPages.length - 1 ? 0 : currentIndex + 1;
 
-  // Select the pagination container
+  // Select pagination container
   const paginationContainer = document.querySelector(".pagination-single");
 
   if (paginationContainer) {
     paginationContainer.innerHTML = `
       <div class="filter-wrapper">
-        <a href="./${projectPages[prevIndex]}" class="text-filter">Previous</a>
+        <a href="${projectPages[prevIndex]}" class="text-filter">Previous</a>
       </div>
       <div class="filter-wrapper-right">
-        <a href="./${projectPages[nextIndex]}" class="text-filter">Next</a>
+        <a href="${projectPages[nextIndex]}" class="text-filter">Next</a>
       </div>
     `;
   }
