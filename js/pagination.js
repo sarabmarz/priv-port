6bee46973d7070b21071bc56cb19ebf4f226da77
saara <132011413+sarabmarz@users.noreply.github.com>
2025-03-23 23:04:22 +0000

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -1,33 +1,39 @@
-// List of project pages inside the "projects" folder
-const projectPages = [
-  "project1.html",
-  "project2.html",
-  "project3.html",
-  "project4.html",
-  "project5.html"
-];
+document.addEventListener("DOMContentLoaded", function () {
+  // List of project pages inside the "projects" folder
+  const projectPages = [
+    "project1.html",
+    "project2.html",
+    "project3.html",
+    "project4.html",
+    "project5.html"
+  ];
 
-// Get the current page filename
-const currentPage = window.location.pathname.split("/").pop();
+  // Get the current page filename
+  const currentPage = window.location.pathname.split("/").pop();
 
-// Find the current project's index
-const currentIndex = projectPages.indexOf(currentPage);
+  // Find the current project's index
+  const currentIndex = projectPages.indexOf(currentPage);
 
-if (currentIndex !== -1) {
-  const prevIndex = currentIndex === 0 ? projectPages.length - 1 : currentIndex - 1;
-  const nextIndex = currentIndex === projectPages.length - 1 ? 0 : currentIndex + 1;
+  if (currentIndex !== -1) {
+    const prevIndex = currentIndex === 0 ? projectPages.length - 1 : currentIndex - 1;
+    const nextIndex = currentIndex === projectPages.length - 1 ? 0 : currentIndex + 1;
 
-  // Select pagination container
-  const paginationContainer = document.querySelector(".pagination-single");
+    // Select pagination container
+    const paginationContainer = document.querySelector(".pagination-single");
 
-  if (paginationContainer) {
-    paginationContainer.innerHTML = `
-      <div class="filter-wrapper">
-        <a href="${projectPages[prevIndex]}" class="text-filter">Previous</a>
-      </div>
-      <div class="filter-wrapper-right">
-        <a href="${projectPages[nextIndex]}" class="text-filter">Next</a>
-      </div>
-    `;
+    if (paginationContainer) {
+      paginationContainer.innerHTML = `
+        <div class="filter-wrapper">
+          <a href="${projectPages[prevIndex]}" class="text-filter">Previous</a>
+        </div>
+        <div class="filter-wrapper-right">
+          <a href="${projectPages[nextIndex]}" class="text-filter">Next</a>
+        </div>
+      `;
+    } else {
+      console.error("Pagination container (.pagination-single) not found!");
+    }
+  } else {
+    console.error("Current page not found in projectPages array.");
   }
-}
+});
