12c40de145d2520bc0c2d92e2428c04c963fb05e
saara <132011413+sarabmarz@users.noreply.github.com>
2025-03-23 22:45:28 +0000

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -4,7 +4,7 @@ const projectPages = [
   "project2.html",
   "project3.html",
   "project4.html",
-  "project5.html",
+  "project5.html"
 ];
 
 // Get the current page filename (without the full path)
@@ -15,19 +15,20 @@ const currentIndex = projectPages.indexOf(currentPage);
 
 if (currentIndex !== -1) {
   // Determine Previous and Next Projects
-  const prevIndex =
-    currentIndex === 0 ? projectPages.length - 1 : currentIndex - 1;
-  const nextIndex =
-    currentIndex === projectPages.length - 1 ? 0 : currentIndex + 1;
+  const prevIndex = currentIndex === 0 ? projectPages.length - 1 : currentIndex - 1;
+  const nextIndex = currentIndex === projectPages.length - 1 ? 0 : currentIndex + 1;
 
   // Select the pagination container
   const paginationContainer = document.querySelector(".pagination-single");
-  paginationContainer.innerHTML = `
-    <div class="filter-wrapper">
-      <a href="../projects/${projectPages[prevIndex]}" class="text-filter">Previous</a>
-    </div>
-    <div class="filter-wrapper-right">
-      <a href="../projects/${projectPages[nextIndex]}" class="text-filter">Next</a>
-    </div>
-  `;
+
+  if (paginationContainer) {
+    paginationContainer.innerHTML = `
+      <div class="filter-wrapper">
+        <a href="./${projectPages[prevIndex]}" class="text-filter">Previous</a>
+      </div>
+      <div class="filter-wrapper-right">
+        <a href="./${projectPages[nextIndex]}" class="text-filter">Next</a>
+      </div>
+    `;
+  }
 }
